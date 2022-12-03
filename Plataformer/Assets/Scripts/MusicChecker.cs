c21e474bd066fc99dd5d8f25e36856765a96dfb1
NeoMakesGames <tomasjuegos@yahoo.com.ar>
2022-12-03 20:11:52 -0300

    Music System Rework

---
+++ Plataformer/Assets/Scripts/MusicChecker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicChecker : MonoBehaviour
+{
+    public GameObject musicManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag("Music").Length == 0)
+        {
+            Instantiate(musicManager);
+        }
+    }
+}
---
+++ Plataformer/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip musicForLevel;
+    private AudioSource musicManager;
+
+    void Start()
+    {
+        musicManager = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+        if(musicManager.clip != musicForLevel)
+        {
+            musicManager.clip = musicForLevel;
+            musicManager.Play();
+        }
+    }
+}
