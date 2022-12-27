7799902cc27c6f6bc4d10537df2215d68311abfd
NeoMakesGames <tomasjuegos@yahoo.com.ar>
2022-12-27 15:50:02 -0300

    Added bloom and changed projectile sprite

---
+++ Plataformer/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public Vector2 interval;
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+    private float timer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        timer = Random.Range(interval.x * 0.1f, interval.y * 0.1f);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+            timer = Random.Range(interval.x * 0.1f, interval.y * 0.1f);
+        }
+    }
+}
