34041eb82ce61a772792e907db8ca4cf2363dd71
NeoMakesGames <tomasjuegos@yahoo.com.ar>
2022-12-30 18:05:20 -0300

    Fixed Main Menu buttons and made Custom Level Loader UI Better

---
+++ Plataformer/Assets/stuff/menuLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuLoader : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
