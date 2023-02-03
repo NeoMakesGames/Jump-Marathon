68738242300ae7be12c6aefd9817cfebeadfe6a7
NeoMakesGames <tomasjuegos@yahoo.com.ar>
2023-02-03 16:03:45 -0300

    Final (maybe) build
    Remade level 1,2 Boss 0, Boss 2; Remade pause menu; Made custom level screen go to fullscreen by default; Added timer to final boss level

--- Plataformer/Assets/stuff/menuLoader.cs
+++ Plataformer/Assets/stuff/menuLoader.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class menuLoader : MonoBehaviour
 {
+    private void Start()
+    {
+        //If scene name == DemoScene then if the game isnt fullscreen make it fullscreen
+        if (SceneManager.GetActiveScene().name == "DemoScene")
+        {
+            if (!Screen.fullScreen)
+            {
+                Screen.fullScreen = true;
+            }
+        }
+    }
+
     public void LoadMenu()
     {
         SceneManager.LoadScene(0);
