6b4dd8061e5f318b8e06a0fa47852f89ac7fc476
NeoMakesGames <tomasjuegos@yahoo.com.ar>
2023-03-16 18:45:20 -0300

    Nivel 3-4 hecho menos random

--- Plataformer/Assets/Scripts/PreFinaleJim.cs
+++ Plataformer/Assets/Scripts/PreFinaleJim.cs
@@ -13,6 +13,9 @@ public class PreFinaleJim : MonoBehaviour
     public float teleportRadius; // radius within which to teleport
     public GameObject teleportParticles; // particle system to emit before teleporting
 
+    public bool fixedTeleports = false;
+    public Transform[] fixedTeleportLocations;
+
     public GameObject projectile;
     private Transform player;
 
@@ -29,50 +32,82 @@ public class PreFinaleJim : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
-        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        if(!fixedTeleports)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
-        {
-            transform.position = this.transform.position;
-
-        }
-        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
-
-        teleportTimer -= Time.deltaTime;
-        if (teleportTimer <= 0)
-        {
-            // Emit particle effect before teleporting
-            Instantiate(teleportParticles, transform.position, Quaternion.identity, transform);
-
-            // Teleport to a random location
-            Vector2 randomPos = (Random.insideUnitCircle * teleportRadius) + (Vector2)player.position;
-            while (Vector2.Distance(randomPos, player.position) < stoppingDistance + 1.5f) {
-                randomPos = (Random.insideUnitCircle * teleportRadius) + (Vector2)player.position;
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
             }
-            transform.position = new Vector3(randomPos.x, randomPos.y, 0);
+            else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+            {
+                transform.position = this.transform.position;
 
-            //Debug.Log("Teleported to " + (player.position.x - randomPos.x) * -1 + ", " + (player.position.y - randomPos.y) * -1);
+            }
+            else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            }
 
-            transform.position = new Vector3(randomPos.x, randomPos.y, 0);
+            teleportTimer -= Time.deltaTime;
+            if (teleportTimer <= 0)
+            {
+                // Emit particle effect before teleporting
+                Instantiate(teleportParticles, transform.position, Quaternion.identity, transform);
+
+                // Teleport to a random location
+                Vector2 randomPos = (Random.insideUnitCircle * teleportRadius) + (Vector2)player.position;
+                while (Vector2.Distance(randomPos, player.position) < stoppingDistance + 1.5f) {
+                    randomPos = (Random.insideUnitCircle * teleportRadius) + (Vector2)player.position;
+                }
+                transform.position = new Vector3(randomPos.x, randomPos.y, 0);
+
+                //Debug.Log("Teleported to " + (player.position.x - randomPos.x) * -1 + ", " + (player.position.y - randomPos.y) * -1);
+
+                transform.position = new Vector3(randomPos.x, randomPos.y, 0);
 
 
-            teleportTimer = teleportInterval;
-        }
+                teleportTimer = teleportInterval;
+            }
 
-        if (timeBtwShots <= 0)
-        {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            timeBtwShots = startTimeBtwShots;
+            if (timeBtwShots <= 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
 
-        }
-        else
-        {
-            timeBtwShots -= Time.deltaTime;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
+        } else {
+            //Teleport to the closest fixed teleport location to the player and instatiate particles. If it's already at the closest location, just shoot. Ignore variables like stoppingDistance and retreatDistance.
+            for(int i = 0; i < fixedTeleportLocations.Length; i++)
+            {
+                if(Vector2.Distance(player.position, fixedTeleportLocations[i].position) < Vector2.Distance(transform.position, player.position))
+                {
+                    if(fixedTeleportLocations[i].position != transform.position)
+                    {
+                        // Emit particle effect before teleporting
+                        Instantiate(teleportParticles, transform.position, Quaternion.identity, transform);
+
+                        // Teleport to the closest fixed teleport location to the player
+                        transform.position = fixedTeleportLocations[i].position;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            if (timeBtwShots <= 0)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
         }
     }
 }
